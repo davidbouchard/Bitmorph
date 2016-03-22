aa29b36b38e14ea683c833d4d6b653f0e84c378d
Lucas Teng <lucas.teng@ryerson.ca>
2016-03-22 11:43:27 -0400

    Added controller for character naming
    Added name_character controller
    removed old assets

--- prototypes/database/table_creation.sql
+++ prototypes/database/table_creation.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS characters
         (
         id MEDIUMINT NOT NULL AUTO_INCREMENT,
-        HEXid TEXT,
-        pri_color TINYINT,
-        sec_color TINYINT,
+        HEXid char(6) unique,
+        customized_name TEXT,
         date_created datetime default '0000-00-00 00:00:00',
         PRIMARY KEY (id)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
@@ -11,20 +10,22 @@ CREATE TABLE IF NOT EXISTS characters
 CREATE TABLE IF NOT EXISTS features
         (
         id MEDIUMINT NOT NULL AUTO_INCREMENT,
-        colFilter_ID TEXT,
+        HEXid char(6) unique,
         sprite_filename TEXT,
+        station_id TEXT,
+        stage TINYINT,
+        side TINYINT,
         PRIMARY KEY (id)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS visits
         (
-        id MEDIUMINT NOT NULL AUTO_INCREMENT,
+        id MEDIUMINT NOT NULL AUTO_INCREMENT,        
         date_posted datetime NOT NULL default '0000-00-00 00:00:00',
-        character_ID MEDIUMINT,
-        feature_ID     MEDIUMINT,
-        svg_path TEXT,
-        audio_path TEXT,
-        FOREIGN KEY (character_ID)  REFERENCES characters(id),
-        FOREIGN KEY (feature_ID)      REFERENCES features(id),
+        current_state TINYINT,
+        character_ID   char(6),
+        feature_ID     char(6),
+        FOREIGN KEY (character_ID)  REFERENCES characters(HEXid),
+        FOREIGN KEY (feature_ID)      REFERENCES features(HEXid),
         PRIMARY KEY (id)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
