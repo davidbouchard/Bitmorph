6deb10bcfcae6374c5fc9c7cb75971e8ee026f5c
Bram <bramwell.su@gmail.com>
2016-04-11 22:52:54 -0400

    Updated Scanner code
    -Fixed the OSC message being sent, able to communicate with processing
    now**
    -added a five second timer to prevent processing from getting swamped
    with new messages

--- prototypes/scanner/Raspberry Pi/Scanner Program V2.py
+++ prototypes/scanner/Raspberry Pi/Scanner Program V2.py
@@ -3,10 +3,11 @@
 import SimpleCV
 import zbar
 import OSC
+import time
 
 from SimpleCV import Color,Camera,Display
 
-send_address = '127.0.0.1' , 9000
+send_address = '141.117.45.114' , 9000
 
 c = OSC.OSCClient()
 c.connect(send_address)
@@ -16,26 +17,42 @@ oldAccount = 0
 cam = Camera()  #starts the camera
 display = Display((640,480)) 
 
+timer = time.time()
+
 while(display.isNotDone()):
  
  img = cam.getImage() #gets image from the camera
 
  barcode = img.findBarcode() #finds barcode data from image
- if(barcode is not None): #if there is some data processed
-   barcode = barcode[0] 
-   result = str(barcode.data)
-   account = result[-6:]
 
-   if (oldAccount != account):
-       print account #prints result of barcode in python shell
-       oldAccount = account
-       msg = OSC.OSCMessage()
-       msg.setAddress("/print")
-       msg.append(account)
+ #print the timer for debugging
+ #print "Timer save at", timer   #show the time we started the timer at
+ #print "Current time:", time.time()    #show the current time passed
+ 
+ if(time.time() > timer+5):
+    
+    print "Timer is up! 5 seconds has passed. Start Scanning!"
+    if(barcode is not None): #if there is some data processed
+       barcode = barcode[0] 
+       result = str(barcode.data)
+       account = result[-6:]
+       #account = result
+       
+       if (oldAccount != account): #check if the passport scanned was the previous one
+           print account #prints result of barcode in python shell
+           oldAccount = account
+           msg = OSC.OSCMessage()
+           msg.setAddress(account)      #add the account number to the msg
+           #msg.append("hello world")
 
-       c.send(msg)
+           c.send(msg)
 
-   barcode = [] #reset barcode data to empty set
-
+           barcode = [] #reset barcode data to empty set
+       else:    #it's the same ticket as before
+           print "Previous ticket detected!", account
+           
+       timer = time.time()
+       print "Reseting timer"
+           
  img.save(display) #shows the image on the screen
  
