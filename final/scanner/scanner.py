6a1b19a6a03de134355177b30cc59d83e845a7e4
davidbouchard <david.bouchard@ryerson.ca>
2016-04-27 14:23:12 -0400

    Added LED toggle n GPIO pin 7 when a code is read

--- final/scanner/scanner.py
+++ final/scanner/scanner.py
@@ -1,84 +1,14 @@
 #!/user/bin/python
 
 import SimpleCV
-import zbar
 import OSC
-import time
 import datetime
 from SimpleCV import Color,Camera,Display
 import argparse
+import RPi.GPIO as GPIO
 
-send_address = '127.0.0.1'
-send_port = 9000
-
-"""
-
-c = OSC.OSCClient()
-c.connect(send_address)
-
-oldAccount = 0
-
-cam = Camera()  #starts the camera
-display = Display((640,480)) 
-
-timer = time.time()
-
-delay = 0  #have delay set to 0 so that it is constantly checking
-
-while(display.isNotDone()):
- 
- img = cam.getImage() #gets image from the camera
-
- barcode = img.findBarcode() #finds barcode data from image
-
- #print the timer for debugging
- #print "Timer save at", timer   #show the time we started the timer at
- #print "Current time:", time.time()    #show the current time passed
- 
- if(time.time() > timer+delay):
-    
-    print "Timer is up! ", delay, " seconds has passed. Start Scanning!"
-    if(barcode is not None): #if there is some data processed
-       barcode = barcode[0] 
-       result = str(barcode.data)
-       account = result[-6:]
-       #account = result
-       
-       if (oldAccount != account): #check if the passport scanned was the previous one
-           print account #prints result of barcode in python shell
-           oldAccount = account
-           msg = OSC.OSCMessage()
-           msg.setAddress("/scan")      #add the account number to the msg
-           #msg.append("hello world")
-           msg.append(account)
-           try:
-             c.s
-             end(msg)
-           except:
-             pass
-
-           barcode = [] #reset barcode data to empty set
-       else:    #it's the same ticket as before
-           print "Previous ticket detected!", account
-
-           ########Comment the following code block if we don't want to send the same account number####
-           oldAccount = account
-           msg = OSC.OSCMessage()
-           msg.setAddress("/scan")      #add the account number to the msg
-           #msg.append("hello world")
-           msg.append(account)
-           try:
-             c.send(msg)
-           except:
-             pass
-           ###################################################################################
-           barcode = [] #reset barcode data to empty set
-           
-       timer = time.time()
-       print "Reseting timer"
-           
- img.save(display) #shows the image on the screen
-"""
+# connected as a sink, so High -> OFF
+ledPin = 7
 
 if __name__ == "__main__":
 
@@ -94,6 +24,11 @@ if __name__ == "__main__":
 
     if args.display:
         display = Display((640, 480))        
+
+    # setup the GPIO pins
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.OUT)
+    GPIO.output(ledPin, True)
 
     # start OSC
     osc = OSC.OSCClient()
@@ -117,6 +52,7 @@ if __name__ == "__main__":
                prevScan = accountID
                print datetime.datetime.now(), "scanned:", accountID
                # turn ON led
+               GPIO.output(ledPin, False)
 
                # send OSC message
                msg = OSC.OSCMessage()
@@ -129,3 +65,5 @@ if __name__ == "__main__":
         else:
             prevScan = None
             # turn OFF led
+            GPIO.output(ledPin, True)
+
